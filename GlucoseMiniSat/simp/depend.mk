32f1f9449f02a4c4c82484bab58c7da56ac1a207
jstrmen <strmen.jakub2@gmail.com>
2018-06-15 10:17:58 +0200

    Adding Glucose (minisat) 4.1

---
+++ GlucoseMiniSat/simp/depend.mk
@@ -0,0 +1,42 @@
+/home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/SimpSolver.o /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/SimpSolver.or /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/SimpSolver.od /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/SimpSolver.op: \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/SimpSolver.cc \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Sort.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Vec.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/IntTypes.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/XAlloc.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../simp/SimpSolver.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Queue.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../core/Solver.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Heap.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Alg.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../utils/Options.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../utils/ParseUtils.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../core/SolverTypes.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Map.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Alloc.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../core/BoundedQueue.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../core/Constants.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Clone.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../core/SolverStats.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../utils/System.h
+/home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/Main.o /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/Main.or /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/Main.od /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/Main.op: \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/Main.cc \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../utils/System.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/IntTypes.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../utils/ParseUtils.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../utils/Options.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Vec.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/XAlloc.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../core/Dimacs.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../core/SolverTypes.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Alg.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Map.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Alloc.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../simp/SimpSolver.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Queue.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../core/Solver.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Heap.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../core/BoundedQueue.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../core/Constants.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../mtl/Clone.h \
+ /home/jstrmen/ulozisko/GraphColoring/SAT/Glucose_minisat/glucose-syrup-4.1/simp/../core/SolverStats.h
